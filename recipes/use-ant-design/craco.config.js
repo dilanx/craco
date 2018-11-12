e78b48d7bdb70c31ed1886252b1f87fd94ffe009
Nathan Broadbent <git@ndbroadbent.com>
2018-11-12 22:51:52 +0700

    Added an example configuration file to set up the babel-import-plugin for Ant Design

---
+++ recipes/use-ant-design/craco.config.js
@@ -0,0 +1,29 @@
+// Install the craco-antd plugin:
+//
+// Yarn:   yarn add craco-antd
+// NPM:    npm i -S craco-antd
+//
+// craco-antd documentation: https://github.com/ndbroadbent/craco-antd
+
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        // You can customize the theme in here, or by creating an
+        // antd.customize.json file in your project's root directory.
+        customizeTheme: {
+          "@primary-color": "#1DA57A",
+          "@link-color": "#1DA57A",
+          "@border-radius-base": "2px"
+        },
+        lessLoaderOptions: {
+          // Any other less-loader options
+          // See: https://webpack.js.org/loaders/less-loader/
+        }
+      }
+    },
+  ],
+};
