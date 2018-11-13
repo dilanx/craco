cefdaae51ea435d7d57fe5a5b82ce2ec5ea6b743
Nathan Broadbent <git@ndbroadbent.com>
2018-11-14 02:17:08 +0700

    Fix one Prettier formatting issue in packages/craco/index.js

--- packages/craco/index.js
+++ packages/craco/index.js
@@ -1,6 +1,20 @@
 const { craPaths } = require("./lib/cra");
-const { getLoader, getLoaders, removeLoaders, addBeforeLoader, addBeforeLoaders, addAfterLoader, addAfterLoaders, loaderByName } = require("./lib/loaders");
-const { when, whenDev, whenProd, whenTest } = require("./lib/user-config-utils");
+const {
+    getLoader,
+    getLoaders,
+    removeLoaders,
+    addBeforeLoader,
+    addBeforeLoaders,
+    addAfterLoader,
+    addAfterLoaders,
+    loaderByName
+} = require("./lib/loaders");
+const {
+    when,
+    whenDev,
+    whenProd,
+    whenTest
+} = require("./lib/user-config-utils");
 const { ESLINT_MODES } = require("./lib/features/eslint");
 const { POSTCSS_MODES } = require("./lib/features/style/postcss");
 
