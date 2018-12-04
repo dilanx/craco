337caea2d4172c83c0bd9cd01e7426c1ff7a4a15
Lafrance <patricklafrance@users.noreply.github.com>
2018-12-04 15:03:16 -0500

    Updated Stylelint recipe

--- recipes/add-stylelint/craco.config.js
+++ recipes/add-stylelint/craco.config.js
@@ -1,9 +1,16 @@
+const path = require("path");
+
+const { paths } = require("@craco/craco");
 const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
     webpack: {
         plugins: [
-            new StyleLintPlugin()
+            new StyleLintPlugin(
+                configBasedir: __dirname,
+                context: path.resolve(__dirname, paths.appSrc),
+                files: ["**/*.css"]
+            )
         ]
     }
 };
