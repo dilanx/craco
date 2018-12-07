dd05e99b05b1bb9a0865b1ad080fb33e9b668546
patrick.lafrance <patrick.lafrance@outlook.com>
2018-12-06 20:59:37 -0500

    Removed useless code since we provide a default config now

--- packages/craco/lib/features/test/jest-babel-transform.js
+++ packages/craco/lib/features/test/jest-babel-transform.js
@@ -3,7 +3,6 @@ const babelJest = require("babel-jest");
 const { loadCracoConfig } = require("../../config");
 const { craPaths } = require("../../cra");
 const { isArray } = require("../../utils");
-const { getJestBabelConfig } = require("./jest-config-utils");
 
 const craBabelTransformer = {
     presets: ["babel-preset-react-app"],
@@ -17,16 +16,24 @@ const context = {
 };
 
 const cracoConfig = loadCracoConfig(context);
+const { addPresets, addPlugins } = cracoConfig.jest.babel;
 
-const { addPresets, addPlugins } = getJestBabelConfig(cracoConfig.jest);
-const { presets, plugins } = cracoConfig.babel;
+if (cracoConfig.babel) {
+    if (addPresets) {
+        const { presets } = cracoConfig.babel;
 
-if (addPresets && isArray(presets)) {
-    craBabelTransformer.presets = presets.concat(craBabelTransformer.presets);
-}
+        if (isArray(presets)) {
+            craBabelTransformer.presets = presets.concat(craBabelTransformer.presets);
+        }
+    }
 
-if (addPlugins && isArray(plugins)) {
-    craBabelTransformer.plugins = plugins;
+    if (addPlugins) {
+        const { plugins } = cracoConfig.babel;
+
+        if (isArray(plugins)) {
+            craBabelTransformer.plugins = plugins;
+        }
+    }
 }
 
 module.exports = babelJest.createTransformer(craBabelTransformer);
