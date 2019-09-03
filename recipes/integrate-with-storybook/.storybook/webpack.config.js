d020045d55729fbb6c50fc150df124866a98f62f
patrick.lafrance <patrick.lafrance@outlook.com>
2019-09-02 20:09:04 -0400

    Remove the storybook recipe

--- recipes/integrate-with-storybook/.storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { createWebpackDevConfig } = require("@craco/craco");
-
-const cracoConfig = require("../craco.config.js");
-
-module.exports = async () => {
-    const webpackConfig = createWebpackDevConfig(cracoConfig);
-
-    return webpackConfig;
-}
--- recipes/integrate-with-storybook/craco.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    webpack: {
-        alias: {
-            "@components": path.resolve(__dirname, "src/components/")
-        }
-    }
-};
