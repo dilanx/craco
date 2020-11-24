a863dae292ec1f9ea01508186444cd8feac4699a
王麟 <viowan@gmail.com>
2020-11-24 22:57:12 +0800

    Support jsx-runtime for jest babel transform

--- packages/craco/lib/features/jest/create-jest-babel-transform.js
+++ packages/craco/lib/features/jest/create-jest-babel-transform.js
@@ -1,10 +1,33 @@
 const babelJest = require("babel-jest");
 
 const { isArray } = require("../../utils");
+/**
+ * To check if support jsx-runtime
+ * Copy from https://github.com/facebook/create-react-app/blob/2b1161b34641bb4d2f269661cd636bbcd4888406/packages/react-scripts/config/jest/babelTransform.js#L12
+ */
+const hasJsxRuntime = (() => {
+    if (process.env.DISABLE_NEW_JSX_TRANSFORM === "true") {
+        return false;
+    }
+
+    try {
+        require.resolve("react/jsx-runtime");
+        return true;
+    } catch (e) {
+        return false;
+    }
+})();
 
 function createJestBabelTransform(cracoConfig) {
     const craBabelTransformer = {
-        presets: ["babel-preset-react-app"],
+        presets: [
+            [
+                "babel-preset-react-app",
+                {
+                    runtime: hasJsxRuntime ? "automatic" : "classic"
+                }
+            ]
+        ],
         babelrc: false,
         configFile: false
     };
